20133546353df6e545e0511e1d9de840f7cc0225
RedstoneChicken <94724551+RedstoneChicken@users.noreply.github.com>
2024-06-21 14:24:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# redstonechicken.com
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/RedstoneChicken/redstonechicken.com)
---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitepress';
+
+// refer https://vitepress.dev/reference/site-config for details
+export default defineConfig({
+  lang: 'en-US',
+  title: 'RedstoneChicken.com',
+  description: 'The official website for Redstone Chicken.',
+
+  themeConfig: {
+    nav: [
+      { text: 'Example', link: '/example' },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+
+    sidebar: [
+      {
+        // text: 'Guide',
+        items: [
+          { text: 'Example', link: '/example' },
+          // ...
+        ],
+      },
+    ],
+  },
+});
